71e75ccd41a731a2f5591c0975bd78813d0ca507
soccc <208098588@qq.com>
2021-04-08 09:28:43 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import router from "./router";
 Vue.config.productionTip = false
 import "./static/style/normalize.css";
 import store from "./store";
-import "./utils/elementui";
+import ElementUI from 'element-ui'
+import { i18n } from './lang'
 
+Vue.use(ElementUI,{
+  i18n: (key, value) => i18n.t(key, value)
+})
 
 new Vue({
+  i18n,
   router,
   store,
   render: h => h(App),
